8d1f523f79b5a817612aec8fe6a57153abc2e269
Buga-00 <cemal.buga@hotmail.com>
2022-12-07 12:09:22 +0100

    Fixing problem with Terraform and S3 bucket

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,5 +5,9 @@ terraform {
       version = "4.40.0"
     }
   }
-
+  backend "s3" {
+    bucket = "analytics-1035"
+    key    = "1035/terraform.state"
+    region = "eu-west-1"
+  }
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,9 @@
 variable "candidate_id" {
+  default = "1035"
   type = string
 }
 
 variable "candidate_email"  {
+  default = "cemal.buga@hotmail.com"
   type = string
 }
